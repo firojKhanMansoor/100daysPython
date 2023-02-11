0a0fd426d47a65dfb53e081c40cbfb544837a174
firoj <itfiroj@gmail.com>
2023-02-11 19:05:39 +0545

    fstring course

--- strings.py
+++ strings.py
@@ -5,3 +5,10 @@ course_name = "Python Programming"
 
 print("our course name is: ", (course_name),
       "overall rating is", (rating), "booking published")
+print("""
+ttis 
+dkfakfj
+kfasjdlkfja
+""")
+print(
+    f"Our Course name is {course_name} overall rating is {rating} booking published.")
