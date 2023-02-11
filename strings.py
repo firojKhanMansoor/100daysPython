2757a64a43ae74623073a99f7c0722b241558878
firoj <itfiroj@gmail.com>
2023-02-11 19:00:44 +0545

    strings works

---
+++ strings.py
@@ -0,0 +1,7 @@
+students_count = 100
+rating = 4.5
+is_published = True
+course_name = "Python Programming"
+
+print("our course name is: ", (course_name),
+      "overall rating is", (rating), "booking published")
