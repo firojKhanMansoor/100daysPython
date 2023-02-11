f011281ad17cc01d5a9f0e1d9a260eb8047e795c
firoj <itfiroj@gmail.com>
2023-02-11 18:43:46 +0545

    variable| String

--- firstday.py
+++ firstday.py
@@ -1,1 +1,8 @@
 print("Hello World")
+a = 1
+b = 2
+c = 5
+e = 5
+print("hello")
+print("5")
+print(a+b)
---
+++ string.py
@@ -0,0 +1,15 @@
+students_count = 100
+rating = 4.5
+is_published = True
+course_name = "Python Programming"
+print(students_count)
+
+print("*" * 30)
+a = 200
+b = 300
+c = (a+b)
+print("a * b +c is equal to ")
+print(a*b+c)
+print(course_name)
+print(len(course_name))
+print(course_name[0:3])
