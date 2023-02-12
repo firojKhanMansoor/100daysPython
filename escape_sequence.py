1fe23d096b4f1495c5bb19ae5eadf23b10320e49
firoj <itfiroj@gmail.com>
2023-02-12 14:18:46 +0545

    Escape Sequence

---
+++ escape_sequence.py
@@ -0,0 +1,12 @@
+# \""
+# \'
+# \\
+# \n
+course = "Python \"Programming"
+print(course)
+course = "Python \'Programming"
+print(course)
+course = "Python \\Programming"
+print(course)
+course = "Python \nProgramming"
+print(course)
