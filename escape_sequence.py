d9a1ba72ba87a039efaaabe28d2faa56b3eca4c6
firoj <itfiroj@gmail.com>
2023-02-12 15:38:26 +0545

    F Strings

--- escape_sequence.py
+++ escape_sequence.py
@@ -10,3 +10,9 @@ course = "Python \\Programming"
 print(course)
 course = "Python \nProgramming"
 print(course)
+
+# formating String
+course_fee = 5000
+course_duration = "3 Months"
+message = f"Dear Students your course name is {course} and it will take time {course_duration} course Fee is :{course_fee}"
+print(message)
---
+++ fstringstest.py
@@ -0,0 +1,6 @@
+# formating String
+course = "Python Programming"
+course_fee = 5000
+course_duration = "3 Months"
+message = f"Dear Students your course name is {course} and it will take time {course_duration} course Fee is :{course_fee}"
+print(message)
