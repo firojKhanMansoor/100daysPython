f98263501c45764a3e38687cceca4601436abf30
firoj <itfiroj@gmail.com>
2023-02-12 18:35:09 +0545

    String Methord

---
+++ stringmethord.py
@@ -0,0 +1,7 @@
+course = "    Python Programming    "
+print(course)
+print(course.upper())  # Upper Case
+print(course.lower())  # loweCase
+print(course.title())
+print(course.strip())
+print(course.rstrip())
