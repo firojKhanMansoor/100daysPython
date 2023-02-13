d6eec1599eb66649322f04b0ec1066b0e9115236
firoj <itfiroj@gmail.com>
2023-02-13 22:06:31 +0545

    tables

---
+++ tabless.py
@@ -0,0 +1,11 @@
+for i in range(1, 11):
+    print("5 x", i, "=", 5*i)
+
+
+name = "John"
+age = 30
+
+# Using f-strings
+print(f"My name is {name} and I am {age} years old.")
+
+# Output: "My name is John and I am 30 years old."
